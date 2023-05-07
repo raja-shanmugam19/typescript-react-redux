2e5199a95825c60d7cb4ad21fec57baec3306bc6
raja-shan <raja.shanmugam@qantas.com.au>
2023-05-08 09:15:32 +1000

    changing to axios and fixing the test case issue

--- src/components/services/UserService.ts
+++ src/components/services/UserService.ts
@@ -1,17 +1,18 @@
 
+import axios from 'axios';
 import { User } from '../models/User';
 import { Post } from '../models/Post';
 import { USERS_API_URL } from '../commons/constants';
 
 export async function fetchUsers(): Promise<User[]> {
-    const response = await fetch(`${USERS_API_URL}/users`);
-    const data = await response.json();
+    const response = await axios.get(`${USERS_API_URL}/users`);
+    const data = await response.data;
     return data;
   }
   
   export async function fetchUserPosts(userId: number): Promise<Post[]> {
-    const response = await fetch(`${USERS_API_URL}/posts?userId=${userId}`);
-    const data = await response.json();
+    const response = await axios.get(`${USERS_API_URL}/posts?userId=${userId}`);
+    const data = await response.data;
     return data;
   }
   
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,31 +1,22 @@
+import { expect } from 'chai';
 
-import { fetchUserPosts, fetchUsers } from "../components/services/UserService";
+import { fetchUsers, fetchUserPosts } from '../components/services/UserService';
 
-describe('fetchUsers', () => {
-  it('returns an array of users', async () => {
-    const users = await fetchUsers();
-    expect(Array.isArray(users)).toBe(true);
-    expect(users.length).toBeGreaterThan(0);
-    expect(users[0]).toHaveProperty('id');
-    expect(users[0]).toHaveProperty('name');
-    expect(users[0]).toHaveProperty('email');
-    expect(users[0]).toHaveProperty('city');
-    expect(users[0]).toHaveProperty('company');
-    expect(users[0].company).toHaveProperty('name');
-  });
+describe('API', () => {
+    describe('fetchUsers()', () => {
+        it('should return an array of users', async () => {
+            const users = await fetchUsers();
+            expect(users).to.be.an('array');
+            expect(users[0].name).to.equal('Leanne Graham');
+        });
+    });
+
+    describe('fetchUserPosts(userId)', () => {
+        it('should return an array of posts for the specified user', async () => {
+            const userId = 1;
+            const posts = await fetchUserPosts(userId);
+            expect(posts).to.be.an('array');
+            expect(posts[0]).to.have.property('userId', userId);
+        });
+    });
 });
-
-describe('fetchUserPosts', () => {
-  it('returns an array of posts for a given user ID', async () => {
-    const userId = 1;
-    const posts = await fetchUserPosts(userId);
-    expect(Array.isArray(posts)).toBe(true);
-    expect(posts.length).toBeGreaterThan(0);
-    expect(posts[0]).toHaveProperty('id');
-    expect(posts[0]).toHaveProperty('userId', userId);
-    expect(posts[0]).toHaveProperty('title');
-    expect(posts[0]).toHaveProperty('body');
-  });
-});
-
-
